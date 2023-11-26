7a7c9c95e5f81befe4b9f67255734a795893cbbc
Oscar Bustos <oscarbustosgarrido@gmail.com>
2023-11-26 01:17:02 +0100

    Refactor webhook handling and update console logs

--- server.js
+++ server.js
@@ -9,20 +9,32 @@ const endpointSecret = process.env.STRIPE_ENDPOINT_SECRET;
 app.post('/webhook', express.raw({type: 'application/json'}), (request, response) => {
   const sig = request.headers['stripe-signature'];
 
+  console.log('Receiving webhook...');
+
   let event;
 
   try {
+    console.log('Constructing event...');
     event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
+    console.log('Event constructed:', event.type);
   } catch (err) {
+    console.error(`Error constructing event: ${err.message}`);
     response.status(400).send(`Webhook Error: ${err.message}`);
     return;
   }
 
-  // Handle the event
-  console.log(`Unhandled event type ${event.type}`);
+  try {
+    // Aquí puedes manejar los diferentes tipos de eventos
+    console.log(`Handling event type ${event.type}`);
+    // ... tu código para manejar los eventos ...
 
-  // Return a 200 response to acknowledge receipt of the event
-  response.send();
+    // Respuesta de éxito para acusar recibo del evento
+    response.status(200).send('Evento recibido');
+  } catch (err) {
+    console.error(`Error handling event ${event.type}: ${err.message}`);
+    response.status(500).send(`Server Error: ${err.message}`);
+  }
 });
 
-app.listen(4242, () => console.log('Running on port 4242'));
+app.listen(4242, () => console.log('Servidor ejecutándose en el puerto 4242'));
+
