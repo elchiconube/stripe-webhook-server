9dbf59000061e25a3825bb6ad68d6a859438fe51
Oscar Bustos <oscarbustosgarrido@gmail.com>
2023-11-26 01:30:51 +0100

    Update webhook endpoint and add event handling logic

--- server.js
+++ server.js
@@ -4,44 +4,43 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const express = require('express');
 const app = express();
 
-const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET;
+const endpointSecret = 'whsec_883bbba51d722444b0126061b0fb347d8ce96f8e513369a76a758987b0c7f43c'
 
-console.log(process.env.STRIPE_SECRET_KEY)
-console.log(process.env.STRIPE_WEBHOOK_SECRET)
+// Middleware para manejar el cuerpo de la solicitud en bruto
+app.use('/webhook', express.raw({type: 'application/json'}));
 
-app.get('/', (req, res) => {
-  res.send('Servidor funcionando correctamente.');
+app.post('/webhook', (request, response) => {
+    const sig = request.headers['stripe-signature'];
+
+    console.log('Webhook received with signature:', sig);
+
+    let event;
+
+    try {
+        event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
+        console.log('Webhook event constructed:', event.type);
+    } catch (err) {
+        console.error(`Error in webhook signature verification: ${err.message}`);
+        return response.status(400).send(`Webhook Error: ${err.message}`);
+    }
+
+    try {
+        // Aquí puedes añadir la lógica para manejar los diferentes tipos de eventos
+        switch (event.type) {
+            case 'checkout.session.completed':
+                console.log('Checkout session completed event received');
+                // Lógica para manejar evento checkout.session.completed
+                break;
+            // Añadir más casos para otros tipos de eventos según sea necesario
+            default:
+                console.warn(`Unhandled event type: ${event.type}`);
+        }
+
+        response.status(200).send('Event processed');
+    } catch (err) {
+        console.error(`Error handling event ${event.type}: ${err}`);
+        response.status(500).send('Server Error');
+    }
 });
 
-app.post('/webhook', express.raw({type: 'application/json'}), (request, response) => {
-  const sig = request.headers['stripe-signature'];
-
-  console.log('Receiving webhook...');
-
-  let event;
-
-  try {
-    console.log('Constructing event...');
-    event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
-    console.log('Event constructed:', event.type);
-  } catch (err) {
-    console.error(`Error constructing event: ${err.message}`);
-    response.status(400).send(`Webhook Error: ${err.message}`);
-    return;
-  }
-
-  try {
-    // Aquí puedes manejar los diferentes tipos de eventos
-    console.log(`Handling event type ${event.type}`);
-    // ... tu código para manejar los eventos ...
-
-    // Respuesta de éxito para acusar recibo del evento
-    response.status(200).send('Evento recibido');
-  } catch (err) {
-    console.error(`Error handling event ${event.type}: ${err.message}`);
-    response.status(500).send(`Server Error: ${err.message}`);
-  }
-});
-
-app.listen(4242, () => console.log('Servidor ejecutándose en el puerto 4242'));
-
+app.listen(4242, () => console.log('Server running on port 4242'));
